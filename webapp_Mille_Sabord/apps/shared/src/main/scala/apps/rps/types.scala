bd3156bab244688978f391938d72050971f8a6cd
Ovoland <oceane.voland@epfl.ch>
2024-11-24 18:22:17 +0100

    Implemented types (object + State + View) for MilleSabord

--- webapp_Mille_Sabord/apps/shared/src/main/scala/apps/rps/types.scala
+++ webapp_Mille_Sabord/apps/shared/src/main/scala/apps/rps/types.scala
@@ -1,62 +1,122 @@
 package apps.rps
 
-import cs214.webapp.UserId
-import Hand.*
+//import cs214.webapp.UserId
+import Dice.*
+import Button.*
+import scala.util.Random
 
-/** Hands are simply strings in this version of the game (use Emoji!) */
-type Hand = String
+type UserId = String
 
-object Hand:
-  val Rock = "✊"
-  val Paper = "✋"
-  val Scissors = "✌️"
-  val allHands = Set(Rock, Paper, Scissors)
+/** Dice are simply strings in this version of the game (use Emoji!) */
+type Dice = String
+type DiceId = Int
 
-extension (hand: Hand)
-  def scoreAgainst(other: Hand): Int = (hand, other) match
-    case (Rock, Scissors) | (Paper, Rock) | (Scissors, Paper) => 1
-    case (Scissors, Rock) | (Rock, Paper) | (Paper, Scissors) => -1
-    case _                                                    => 0
+/** Button are the string with the indication text of the button */
+type Button = String
+type ButtonId = Int
 
-/** A view of the rock paper scissor's state for a specific client.
+object Dice:
+  val Skull = "💀"
+  val Diamond = "💎"
+  val Coin = "📀"
+  val Sword = "🔪"
+  val Monkey = "🐵" //"🐒"
+  val Parrot = "🐦"
+
+extension (dice: Dice)
+  def randomDice(self: Dice): Dice=
+    //Get a random number between the interval [1,7[
+    val randomDiceIdx = Random.between(1, 7)
+    randomDiceIdx match 
+      case 1 => Dice.Skull
+      case 2 => Dice.Diamond
+      case 3 => Dice.Coin
+      case 4 => Dice.Sword
+      case 5 => Dice.Monkey
+      case 6 => Dice.Parrot
+
+
+object Button:
+  val Roll = "Roll the dice"
+  val End = "End my turn"
+
+/** A view of Mille Sabord's state for a specific client.
   *
-  * The UI alternates between two views: selecting next hand and viewing the
-  * results, attributing corresponding scores.
+  * The UI alternates between two views: 
+  * - the "playing" phase where the player select dice, reroll them and can end its turn
+  * - the "scoring" phase where the player turn ends and its points for the turn are computed and displayed
   *
-  * @param phaseView
+  * @param stateView
   *   A projection of the current phase of the game.
   * @param scoresView
   *   The score of each player.
   */
 case class View(
-    phaseView: PhaseView,
+    stateView: StateView,
     scoresView: ScoresView
 )
 
-enum PhaseView:
-  /** Players are selecting their next hand. */
-  case SelectingHand(ready: Map[UserId, Boolean])
+enum StateView:
+  /** The game is ongoing. */
+  case Playing(phase: PhaseView, currentPlayer: UserId, dice: Vector[DiceView], button: Vector[ButtonView])
 
-  /** Players are looking at each other's hand. */
-  case ViewingHands(hands: Map[UserId, Hand])
+  /** The game is over (only one winner of the game possible) */
+  case Finished(winnerId: UserId)
+
+enum PhaseView:
+  /** It's our turn to select dice to rethrown. */
+  case SelectingDice
+
+  /** It's another player's turn: we're waiting for them rethrow dice or stop. */
+  case Waiting
+
+  /** Your turn came to an end because you get 3 skulls */
+  case SkullEnd
+
+  /** Your turn came to an end because you chose to stop it and save your score*/
+  case SavingEnd
 
 type ScoresView = Map[UserId, Int]
 
-enum Event:
-  /** A player has chosen their hand. */
-  case HandSelected(hand: Hand)
 
-// ----------- Do we need to use type State ? ----------
-//type State = Unit
+enum DiceView:
+  /** Selected dice have a green square around them*/
+  case Selected(dice: Dice)
+
+  /** Unselected dice just have default view*/
+  case Unselected(dice: Dice)
+
+  /** Skull dice have a gray square around them and a lower opacity*/
+  case Skull(dice: Dice)
+
+
+enum ButtonView: 
+  case Clickable(button: Button)
+
+  /** NonClickable button can't be selected and have a lower opacity*/
+  case NonClickable(button: Button)
+
+  /** Selected buttons have a small animation when selected*/
+  case Selected(button: Button)
+
+enum Event:
+  /** A player has selected a dice. */
+  case DiceClicked(diceId: DiceId)
+
+  /** A player has clicked on a button. */
+  case ButtonClicked(buttonId: ButtonId)
+
+
 enum Phase:
-  case SelectingHand
-  case ViewingHands
+  case SelectingDice
+  /** View when the player is rolling dice. */
+  case ViewingDice 
   case Done
 
 case class State (  
   players: Vector[UserId],
   phase: Phase,
-  versusHands : Map[UserId, Hand],
-  score: Map[UserId, Int],
-  round: Int
+  dice : Vector[Dice],
+  selectedDice : Set[DiceId],
+  score: Map[UserId, Int]
 )
